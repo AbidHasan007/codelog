9372428b4643c3497f9a667e7242d70a3101e2dc
Abid Hasan <ahmasum890@gmail.com>
2022-08-28 07:00:00 +0000

    UVa 10071 back to high school physics

---
+++ cpcode/Uva10071.cpp
@@ -0,0 +1,11 @@
+//UVa 10071 back to high school physics
+#include<bits/stdc++.h>
+using  namespace std;
+int main()
+{
+  int v,t;
+  while(scanf("%d %d",&v,&t) != EOF){
+    printf("%d",2*v*t);
+  }
+    return 0;
+}
